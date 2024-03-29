7ec4925dcad5b1856b2de6e46311c8a5196b7f62
Christopher <christopher.geever@gmail.com>
2024-03-28 21:42:22 -0400

    scroll navigation

--- src/components/NaviagationBar.jsx
+++ src/components/NaviagationBar.jsx
@@ -1,12 +1,21 @@
 import './styles/NavigationBar.css'
 
 export default function NavigationBar(){
+    const smoothScroll = (event, targetId) => {
+        event.preventDefault();
+        const targetElement = document.getElementById(targetId);
+        window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: 'smooth'
+        });
+      };
     return(
+        
         <nav className="navigation-container">
             <ul>
-                <li><a href="/home">Home</a></li>
-                <li><a href="/usage">Usage</a></li>
-                <li><a href="/devteam">DevTeam</a></li>
+                <li><a href="#home" onClick={(e) => smoothScroll(e,'home')}>Home</a></li>
+                <li><a href="#usage" onClick={(e) => smoothScroll(e,'usage')}>Usage</a></li>
+                <li><a href="#devteam" onClick={(e) => smoothScroll(e,'devteam')}>DevTeam</a></li>
             </ul>
         </nav>
     )
--- src/screens/Welcome.jsx
+++ src/screens/Welcome.jsx
@@ -1,18 +1,15 @@
 // Welcome.jsx
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link, NavLink } from "react-router-dom";
-import Particles, { initParticlesEngine } from "@tsparticles/react";
-import { loadSlim } from "@tsparticles/slim";
 
 import './styles/Welcome.css'
-import ImageContainer from '../components/ImageContainer';
 import NavigationBar from '../components/NaviagationBar';
 import { Button } from '../components/CustomButton';
 import ParticlesBackground from './Particles';
 
 const Welcome = () => {
-
+    const ref = useRef();
     return (
             <>
             <header>
@@ -47,8 +44,56 @@ const Welcome = () => {
         
         </header>
 
-        <section className='inline'>
-            helloooo
+        <section id={'home'} className='content inline'>
+                <div className='grid-item'>
+                    <img src="/upper-photo.png" alt="" />
+                </div>
+                <div className='grid-item'>
+                    <h1>
+                        What is Tag Along?
+                    </h1>
+                    <p className='description'>
+                    A transformative solution designed to bridge the transportation gap within university communities.
+
+                    Tag Along provides a dynamic platform where members with vehicles can seamlessly connect with those in need of transportation.
+                    </p>
+                </div>
+
+        </section>
+
+        <section id='usage' className='content inline iceblue-back'>
+                
+                <div className='grid-item'>
+                    <h1>
+                        Usage
+                    </h1>
+                    <p className='description'>
+
+                    A user with a car posts details about an upcoming journey, including the starting point, destination, date, fuel price, available seats, car type, and luggage space.
+
+                    Meanwhile, fellow members can effortlessly search for rides matching their destinations, assess the date and pricing compatibility, and simply tag along by sending a request.
+
+                    </p>
+                </div>
+
+                <div className='grid-item'>
+                    <img src="/lower-photo.png" alt="" />
+                </div>
+
+        </section>
+
+        <section id='devteam' className='content orange-back'>
+                
+            <h1>Meet the Team</h1>
+            <img src="/developers.png" style={{position:'relative'}} alt="" />
+
+        </section>
+
+        <section className='content dark-blue-back' style={{position:'relative'}}>
+                
+            <h1>Footer</h1>
+            
+
         </section>
         </>
     );

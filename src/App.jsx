fc80739d7692a84147c14acbb4a63379944e1106
Christopher <christopher.geever@gmail.com>
2024-04-21 23:12:11 -0400

    hot fixes loading issue

--- src/App.jsx
+++ src/App.jsx
@@ -110,14 +110,9 @@ const router = createBrowserRouter([
     }
 ]);
 
-const { isLoaded } = useLoadScript({
-  googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY, // Using Vite's env variable syntax
-  libraries: ['places'],
-});
-
-if (!isLoaded) return <div>Loading...</div>;
 
 function App() {
+  
   return(
     <AuthProvider>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
